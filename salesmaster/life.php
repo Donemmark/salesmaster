fd012607b4ac6018d29b43853d7ab9ab776f0272
Donemmark <emmanuelolorunwa22@gmail.com>
2024-06-21 10:43:22 +0100

    Add files via upload

---
+++ salesmaster/life.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+?>
+
+
+
+
+
+
+<html>
+<body>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select file to upload:
+    <input type="file" name="fileToUpload" id="fleToUpload">
+    <input type="submit" value="Upload File" name="submt">
+
+</form>
+
+
+</body>
+</html>
